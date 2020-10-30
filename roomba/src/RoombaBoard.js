b768a575e8f5f8d9e9948e748cc049c192c9e13f
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-10-30 18:03:13 -0500

    added more functionality for rumba and renders result

--- roomba/src/RoombaBoard.js
+++ roomba/src/RoombaBoard.js
@@ -1,106 +1,171 @@
 import React, { useState } from 'react';
-// import json from '../src/app.json'
-import roombaFn from './roomba.js'
 import Blank from './blank.png';
-// import Roomba from './roomba.jpg';
-import Green from './green.jpg'
-
-const kickOff = (json) => {
-    console.log(typeof(json), "sdfsfsdf");
-    console.log(roombaFn(json[0]));
-}
-
-const parseFile  = (files, callBack) => {
-    var reader = new FileReader();
-    reader.readAsText(files[0]);
-    reader.onload = function(e) {
-        callBack(JSON.parse(reader.result));
-  }
-  
-}
+import Roomba from './roomba.jpg';
+import Table from 'react-bootstrap/Table'
 
 
 
 const RoombaBoard = () => {
-// const width=10;
-// const height=10;
     let initialRows = [];
-    for(let i =0; i<10; i++){
-        initialRows.push([]);
-        for(let k=0; k<10; k++){
-            initialRows[i].push('blank')
+    let res= {}
+
+
+    function kickOff (json){
+        let newState = []
+
+        for(let i =0; i<json[0].roomDimensions[0]; i++){
+            newState.push([]);
+            for(let k=0; k<json[0].roomDimensions[1]; k++){
+                if(i === json[0].initialRoombaLocation[0] && k === json[0].initialRoombaLocation[1]) {
+                    newState[i].push('roomba')
+                } else {
+                    newState[i].push('blank')
+                }
+            }
         }
+
+        setRows(newState);
+        let r = roomba(json[0], newState);
+        moveRoomba(newState, r.roombaSteps);
+    }
+    
+    
+    
+    const parseFile  = (files, callBack) => {
+        var reader = new FileReader();
+        reader.readAsText(files[0]);
+        reader.onload = function(e) {
+            callBack(JSON.parse(reader.result), 'json');
+      }
+      
     }
 
+    function moveRoomba (initial, arr) {
+           for(let i = 0; i< arr.length; i++) {
+            setTimeout(function() {
+                let temp = initial
+                temp[arr[i][0]][arr[i][1]]="roomba";
+                setRows(temp);
+                console.log("moving", i, temp)
+            }, 10);
+           }
+    }
+    
+
 const [rows, setRows] = useState(initialRows);
-const [roomba, setRoomba] = useState([{x:0,y:0}]);
-
-const displayRoomba = () => {
-
-const newRows = rows;
-
-roomba.forEach(cell => {
-
-newRows[cell.x][cell.y]='roomba';
-console.log(newRows, 'rows')
-
-})
-
-setRows(newRows);
-
-}
-const moveRoomba = () => {
-    const newRoomba = [];
-    // switch(direction) {
-    //     case 'right':
-    //         newRoomba.push({x: roomba[0].x, y: (roomba[0].y + 1)%width})
-    //         break;
-    //     case 'left':
-    //         newRoomba.push({x: roomba[0].x, y: (roomba[0].y - 1 + width)%width})
-    //         break;
-    //     case 'top':
-    //         newRoomba.push({x: (roomba[0].x - 1 + height)%height, y: roomba[0].y})
-    //         break;
-    //     case 'bottom':
-    //         newRoomba.push({x: (roomba[0].x + 1)%height, y: roomba[0].y})
-    // }
-    //     roomba.forEach(cell=> {
-    //         newRoomba.push(cell);
-    //     })    
-    // if(roomba[0].x === food.x && roomba[0].y === food.y) {
-    //     setFood(randomPosition);
-    // }else {
-    //     newRoomba.pop();
-    // }
-    setRoomba(newRoomba);
-    displayRoomba();
-}
+const [result, setResult] = useState(res);
 
 
 const displayRows = rows.map(row => 
     <li style={{listStyleType: "none"}}>
+        
         {row.map(e => {
             switch(e) {
                 case 'blank':
                     return<img src={Blank} style={{width: 70, height: 70, padding:0}}/>
                 case 'roomba':
-                return <img src={Green} style={{width: 70, height: 70, padding:0}}/>
+                return <img src={Roomba} style={{width: 70, height: 70, padding:0}}/>
             }
         })
     }
     </li>
 
 )
+ function roomba(json, newState){
+     console.log()
+    let dirts =0;
+    let roombaLocation;
+    let wallHit =0;
+  
+  
+    let dirtMap ={};
+  
+    for(let i = 0; i< json.dirtLocations.length; i++){
+      dirtMap[json.dirtLocations[i]] = true;
+    }
+
+    let k =0;
+    let i =json.initialRoombaLocation[0];
+    let j =json.initialRoombaLocation[1];
+    let distanse;
+    let roombaSteps = [];
+  
+  
+  while(k < json.drivingInstructions.length) {
+    
+       console.log(json.drivingInstructions[k], 'k')
+  
+       if(dirtMap[i+","+j]){
+         console.log(dirts, i+","+j)
+          dirts+=1;
+       }
+  
+        if (json.drivingInstructions[k] === 'N' && j === json.roomDimensions[1] || json.drivingInstructions[k] === 'E'&& i === json.roomDimensions[0] || json.drivingInstructions[k] === 'S' && j === 0 || json.drivingInstructions[k] === 'W' && i === 0){
+          wallHit +=1;
+        }else{
+          if(json.drivingInstructions[k] === 'N'){
+            j++;
+          }
+          if(json.drivingInstructions[k] === 'E'){
+            i++;
+          }
+          if(json.drivingInstructions[k] === 'S'){
+            j--
+          }
+          if(json.drivingInstructions[k] === 'W'){
+            i--
+          }
+        }
+        k++;
+        roombaSteps.push([i,j])
+         roombaLocation = [i,j]
+         distanse = json.drivingInstructions.length - wallHit;
+  
+        console.log(wallHit, 'wallhit here')
+     
+    }
+    let lastResult =   {
+        'Total Distance Traveled:': distanse,
+        'Roomba Location': roombaLocation, 
+        'Total Dirt Collacted': dirts,
+        'Total WAll Hit':wallHit,
+        "roombaSteps" : roombaSteps
+    }
+
+    console.log(lastResult, 'res')
+    setResult(lastResult);
+  return lastResult;
+}
+
 
 return (
+    <>
     <div>
         <input type="file" accept=".json" onChange={e => {
           parseFile([...e.target.files], kickOff);
         }}/>
-        { displayRows}
-        {/* {displayRoomba} */}
+        { displayRows}  
     </div>
+    <div>
+    {Object.keys(result).length > 0 ? Object.entries(result).map((t,k) => <li style={{listStyleType: "none"}}  key={k} value={t[0]}>{t[1]}</li>) : "NOTHING"}
+
+    
+    {/* <Table striped bordered hover size="sm">         
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+
+</Table> */}
+    </div>
+
+    </>
 )
+
 }
 
 
